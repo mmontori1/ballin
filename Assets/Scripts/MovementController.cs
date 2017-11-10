f0344587ef9b2ae067e504d14dd1a46933115826
Mariano <mmontori@umich.edu>
2017-11-09 20:59:27 -0500

    doin it simple

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+
+	public float speed;
+	private float input;
+
+	void Start(){
+		speed = 3.5f;
+	}
+
+	void Update(){
+		input = Input.GetAxisRaw ("Horizontal");
+		transform.Rotate (Vector3.forward * -input * speed);
+	}
+}

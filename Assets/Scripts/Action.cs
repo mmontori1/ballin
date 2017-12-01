d455d6be1d7543947c533698798bfc598dd04094
Mariano <mmontori@umich.edu>
2017-12-01 12:17:44 -0500

    better direction on an input system based off actions

---
+++ Assets/Scripts/Action.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Action {
+	public Action(KeyCode newInput){
+		input = newInput;
+	}
+
+	private KeyCode input;
+	private bool press;
+
+	public delegate void Act();
+	public Act action;
+
+	public void Perform(){
+		press = Input.GetKeyDown(key: input);
+		if (action != null && press) {
+			action();
+		}
+	}
+}
--- Assets/Scripts/ActionController.cs
+++ Assets/Scripts/ActionController.cs
@@ -9,23 +9,51 @@ public class ActionController : MonoBehaviour {
 	private float speed;
 	private bool input;
 
+	private Action space;
+
 	void Start () {
 		speed = 10f;
 		generator = GetComponent<BallGenerator>();
 		nextColor = GetComponent<NextColor>();
+
+		space = new Action(KeyCode.Space);
+		space.action = forward;
 	}
 	
 	void Update () {
-		input = Input.GetKeyDown(KeyCode.Space);
-		if (input) {
-			GameObject newBall = generator.generate();
-
-			Ball ball = newBall.GetComponent<Ball>();
-			ball.setColor(nextColor.color);
-			nextColor.randColor();
-
-			Rigidbody2D rb = newBall.GetComponent<Rigidbody2D>();
-			rb.velocity = transform.up * speed;
-		}
+		setAction();
+		space.Perform();
 	}
+
+	void setAction(){
+		bool isWhite = (nextColor.color == Color.white);
+		if(isWhite){
+			space.action = mirror;
+		}
+		else {
+			space.action = forward;
+		}
+	}				
+
+	void shoot(int direction){
+		GameObject newBall = generator.generate ();
+
+		Ball ball = newBall.GetComponent<Ball> ();
+		ball.setColor (nextColor.color);
+		nextColor.randColor ();
+
+		Rigidbody2D rb = newBall.GetComponent<Rigidbody2D> ();
+		rb.velocity = transform.up * speed * direction;
+	}
+
+	void forward(){
+		shoot(1);
+	}
+
+	void mirror(){
+		shoot(1);
+		shoot(-1);
+	}
+
+
 }
